bcbf0715599b7390ad06f3d3538afec6c95c2988
yangyazhou <1628520112@qq.com>
2018-03-19 00:15:04 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 import "./css/reset.css";
 import "./sass/index.scss";
-import $ from "jquery";
-//myself-select
-$(document).ready( function(){
-
-})
---
+++ src/js/Symbol.js
@@ -0,0 +1,4 @@
+let name = Symbol('name');
+let name2 = Symbol('name');
+
+console.log(name === mame2)
